2877b653adabf43b2dc1efe50ed4687400080b74
osxmidi <chopinbig@tutanota.com>
2020-05-16 14:13:25 +1000

    Update TabPresetEditor.cpp

--- Source/gui/TabPresetEditor.cpp
+++ Source/gui/TabPresetEditor.cpp
@@ -447,6 +447,8 @@ void TabPresetEditor::paint (Graphics& g)
     //[/UserPrePaint]
 
     g.fillAll (Colour (0xffffe000));
+	
+    updateListBox();	
 
     //[UserPaint] Add your own custom painting code here..
     //[/UserPaint]
